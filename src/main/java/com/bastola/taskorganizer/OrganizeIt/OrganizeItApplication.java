2bdae87ce5e0c2a70f947e55e643a2d27b68ff54
Bibek Bastola <dev.bibekbastola@gmail.com>
2024-10-08 19:40:22 -0500

    Initial commit

---
+++ src/main/java/com/bastola/taskorganizer/OrganizeIt/OrganizeItApplication.java
@@ -0,0 +1,13 @@
+package com.bastola.taskorganizer.OrganizeIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizeItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastola/taskorganizer/OrganizeIt/OrganizeItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastola.taskorganizer.OrganizeIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizeItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
